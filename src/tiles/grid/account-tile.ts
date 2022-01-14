1b27bf6371529fc5d10e61c9618eb51cce7b4223
banazsinjary <banazsinjary@gmail.com>
2022-01-14 11:04:35 -0800

    Tile view and design set up

--- src/tiles/grid/account-tile.ts
+++ src/tiles/grid/account-tile.ts
@@ -8,16 +8,66 @@ export class UserTile extends LitElement {
 
   render() {
     return html`
-      <h1>User</h1>
-      <h2>${this.model?.title}</h2>
+      <div class="outter-holder">
+        <div class="title-holder">
+          <h1>User Profile</h1>
+        </div>
+        <div class="avatar-holder">
+          <div class="avatar"></div>
+        </div>
+        <div class="credit-holder">
+          <h2>${this.model?.title}</h2>
+        </div>
+        <div class="status-holder"></div>
+      </div>
     `;
   }
 
   static get styles() {
     return css`
       h1 {
-        color: green;
+        color: black;
+        font-size: 16px;
         margin-top: 0;
+      }
+
+      .outter-holder {
+        background-color: #fcf5e6;
+        border: 1px #2c2c2c;
+        border-radius: 4px;
+        box-shadow: 1 1 2 0;
+        height: auto;
+        text-align: center;
+        padding: 5px;
+      }
+
+      .title-holder {
+        background-color: pink;
+      }
+
+      .avatar-holder {
+        background-color: gray;
+      }
+
+      .avatar {
+        background-color: white;
+        border-radius: 50%;
+        height: 10vh;
+        width: 10vw;
+        display: inline-block;
+        margin: 5px;
+      }
+
+      .credit-holder {
+        background-color: greenyellow;
+        height: 30px;
+        margin-bottom: 5px;
+      }
+
+      .status-holder {
+        background-color: rebeccapurple;
+        height: 30px;
+        margin-bottom: 5px;
       }
     `;
   }
