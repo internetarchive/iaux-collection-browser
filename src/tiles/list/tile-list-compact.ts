bfeace6d03e8b648ed38cfdc7fac51b46b95f68c
Mark Carranza <mark.carranza@gmail.com>
2022-03-02 14:35:15 -0800

    Rebase for new date vars

--- src/tiles/list/tile-list-compact.ts
+++ src/tiles/list/tile-list-compact.ts
@@ -1,19 +1,31 @@
 import { css, html, LitElement } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
+import { ifDefined } from 'lit/directives/if-defined.js';
 import { TileModel } from '../../models';
+// import { collectionIcon } from '../../assets/img/icons/collection';
 
 @customElement('tile-list-compact')
 export class TileListCompact extends LitElement {
   @property({ type: Object }) model?: TileModel;
 
+  @property({ type: String }) baseNavigationUrl?: string;
+
   render() {
+    // Todo: Manage different date types
     return html`
       <div id="list-compact">
         <div id="views">${this.model?.viewCount}</div>
-        <div id="title">${this.model?.title}</div>
+        <div id="title">
+          <a
+            href="${this.baseNavigationUrl}/details/${this.model?.identifier}"
+            title=${ifDefined(this.model?.title)}
+          >
+            ${this.model?.title}
+          </a>
+        </div>
         <div id="date-published">${this.model?.datePublished?.toDateString()}</div>
         <div id="creator">${this.model?.creator}</div>
-        <div id="icon">${this.model?.mediatype}</div>
+        <div id="icon">icon</div>
       </div>
     `;
   }
@@ -22,14 +34,15 @@ export class TileListCompact extends LitElement {
   static get styles() {
     return css`
       #list-compact {
-        display: table-row;
-        border-spacing: 5px 10px;
-        border-top: 1px solid #ddd;
-        text-align: left;
-        overflow-x: hidden;
-      }
-      #list-compact div {
-        display: table-cell;
+        display: grid;
+        grid-template-columns: 80px auto 115px 278px 22px;
+        column-gap: 10px;
+
+        font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+        font-size: 14px;
+        line-height: 1.42857143;
+        color: #333;
+        background-color: #fff;
       }
 
       h1 {
