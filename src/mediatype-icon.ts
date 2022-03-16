16d53ca91da581aebf400499dd178ec64bb83c5a
Clau <1281581+dualcnhq@users.noreply.github.com>
2022-03-17 06:56:03 +0800

    Mediatype icons selection component (#8)
    * Add mediatype icon selection component
    
    * Refactor rendering media type icons object
    
    * Change classname and defined private readonly value for icons
    
    * Rename file
    
    * Change classname from camelCase format
    
    * Update styling and stats text display
    
    * Remove unused div style class
    
    * Delete dupe component

---
+++ src/mediatype-icon.ts
@@ -0,0 +1,71 @@
+import { css, CSSResultGroup, html, LitElement } from 'lit';
+import { customElement, property } from 'lit/decorators.js';
+
+import { audioIcon } from './assets/img/icons/mediatype/audio';
+import { etreeIcon } from './assets/img/icons/mediatype/etree';
+import { imagesIcon } from './assets/img/icons/mediatype/images';
+import { filmIcon } from './assets/img/icons/mediatype/film';
+import { softwareIcon } from './assets/img/icons/mediatype/software';
+import { textsIcon } from './assets/img/icons/mediatype/texts';
+import { tvIcon } from './assets/img/icons/mediatype/tv';
+import { videoIcon } from './assets/img/icons/mediatype/video';
+import { webIcon } from './assets/img/icons/mediatype/web';
+
+@customElement('mediatype-icon')
+export class MediatypeIcon extends LitElement {
+  @property({ type: String }) mediatype = '';
+
+  private readonly mediatypeIcons: { [key: string]: any } = {
+    audio: audioIcon,
+    data: etreeIcon,
+    etree: etreeIcon,
+    film: filmIcon,
+    image: imagesIcon,
+    movies: filmIcon,
+    software: softwareIcon,
+    texts: textsIcon,
+    tv: tvIcon,
+    video: videoIcon,
+    web: webIcon,
+  };
+
+  private readonly mediatypeText: { [key: string]: any } = {
+    audio: 'Audio',
+    data: 'Data',
+    etree: 'E-tree',
+    film: 'Film',
+    image: 'Image',
+    movies: 'Movie',
+    software: 'Software',
+    texts: 'Text',
+    tv: 'TV',
+    video: 'Video',
+    web: 'Web',
+  };
+
+  render() {
+    return html`
+      <div>
+        ${this.mediatypeIcons[this.mediatype]}
+        <p class="status-text">${this.mediatypeText[this.mediatype]}</p>
+      </div>
+    `;
+  }
+
+  static get styles(): CSSResultGroup {
+    return css`
+      .status-text {
+        font-size: 14px;
+        color: #2c2c2c;
+        margin: auto;
+        display: block;
+        text-align: center;
+      }
+
+      svg {
+        height: 10px;
+        width: 10px;
+      }
+    `;
+  }
+}
