1de1a36507a7da354160920a640c6cbe2ff8ebe3
Clau Yagyagan <1281581+dualcnhq@users.noreply.github.com>
2022-03-16 20:56:20 +0800

    Update styling and stats text display

--- src/tiles/grid/mediatype-icons.ts
+++ src/tiles/grid/mediatype-icons.ts
@@ -1,15 +1,15 @@
 import { css, CSSResultGroup, html, LitElement } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 
-import { audioIcon } from './assets/img/icons/mediatype/audio';
-import { etreeIcon } from './assets/img/icons/mediatype/etree';
-import { imagesIcon } from './assets/img/icons/mediatype/images';
-import { filmIcon } from './assets/img/icons/mediatype/film';
-import { softwareIcon } from './assets/img/icons/mediatype/software';
-import { textsIcon } from './assets/img/icons/mediatype/texts';
-import { tvIcon } from './assets/img/icons/mediatype/tv';
-import { videoIcon } from './assets/img/icons/mediatype/video';
-import { webIcon } from './assets/img/icons/mediatype/web';
+import { audioIcon } from '../../assets/img/icons/mediatype/audio';
+import { etreeIcon } from '../../assets/img/icons/mediatype/etree';
+import { imagesIcon } from '../../assets/img/icons/mediatype/images';
+import { filmIcon } from '../../assets/img/icons/mediatype/film';
+import { softwareIcon } from '../../assets/img/icons/mediatype/software';
+import { textsIcon } from '../../assets/img/icons/mediatype/texts';
+import { tvIcon } from '../../assets/img/icons/mediatype/tv';
+import { videoIcon } from '../../assets/img/icons/mediatype/video';
+import { webIcon } from '../../assets/img/icons/mediatype/web';
 
 @customElement('mediatype-icon')
 export class MediatypeIcon extends LitElement {
