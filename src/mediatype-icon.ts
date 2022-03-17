b2e46457295bd6c40141289dce465df63bbfe8e0
Mark Carranza <mark.carranza@gmail.com>
2022-03-16 18:02:37 -0700

    rebase from initial_work

--- src/mediatype-icon.ts
+++ src/mediatype-icon.ts
@@ -10,6 +10,7 @@ import { textsIcon } from './assets/img/icons/mediatype/texts';
 import { tvIcon } from './assets/img/icons/mediatype/tv';
 import { videoIcon } from './assets/img/icons/mediatype/video';
 import { webIcon } from './assets/img/icons/mediatype/web';
+import { collectionIcon } from './assets/img/icons/mediatype/collection';
 
 @customElement('mediatype-icon')
 export class MediatypeIcon extends LitElement {
@@ -17,8 +18,11 @@ export class MediatypeIcon extends LitElement {
 
   @property({ type: Boolean }) showText = false;
 
+  @property({ type: String }) icontype = 'tile';
+
   private readonly mediatypeIcons: { [key: string]: any } = {
     audio: audioIcon,
+    collection: collectionIcon,
     data: etreeIcon,
     etree: etreeIcon,
     film: filmIcon,
@@ -33,6 +37,7 @@ export class MediatypeIcon extends LitElement {
 
   private readonly mediatypeText: { [key: string]: any } = {
     audio: 'Audio',
+    collection: 'Collection',
     data: 'Data',
     etree: 'E-tree',
     film: 'Film',
