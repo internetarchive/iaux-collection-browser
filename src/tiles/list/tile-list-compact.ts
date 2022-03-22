ecb36277e1c7cc9343c99d57e074c8c0e482fd48
Mark Carranza <mark.carranza@gmail.com>
2022-03-22 14:33:40 -0700

    Temp dupe into details

--- src/tiles/list/tile-list-compact.ts
+++ src/tiles/list/tile-list-compact.ts
@@ -1,6 +1,5 @@
 import { css, html, LitElement } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
-import { ifDefined } from 'lit/directives/if-defined.js';
 import { SortParam } from '@internetarchive/search-service';
 import { TileModel } from '../../models';
 import { formatCount, NumberFormat } from '../../utils/format-count';
@@ -25,20 +24,12 @@ export class TileListCompact extends LitElement {
   @property({ type: Object }) sortParam?: SortParam;
 
   render() {
-    // Todo: Manage different date types
     return html`
       <div id="list-compact" class="${this.classSize}">
         <div id="views">
           ${formatCount(this.model?.viewCount ?? 0, this.formatSize)}
         </div>
-        <div id="title">
-          <a
-            href="${this.baseNavigationUrl}/details/${this.model?.identifier}"
-            title=${ifDefined(this.model?.title)}
-          >
-            ${this.model?.title}
-          </a>
-        </div>
+        <div id="title">${this.model?.title}</div>
         <div id="date">${formatDate(this.date, this.formatSize)}</div>
         <div id="creator">${this.model?.creator}</div>
         <div id="icon">
@@ -77,7 +68,7 @@ export class TileListCompact extends LitElement {
   static get styles() {
     return css`
       .mobile div {
-        font-size: 9px;
+        font-size: 11px;
       }
       .desktop div {
         font-size: 14px;
@@ -85,7 +76,6 @@ export class TileListCompact extends LitElement {
 
       #list-compact {
         display: grid;
-        grid-template-columns: 60px 3fr 90px 2fr 29.5px;
         column-gap: 10px;
         line-height: 1.42857143;
         border-top: 1px solid #ddd !important;
@@ -93,18 +83,20 @@ export class TileListCompact extends LitElement {
         align-items: center;
       }
 
-      h1 {
-        margin-top: 0;
+      #list-compact.mobile {
+        grid-template-columns: 33px 3fr 30px 2fr 29.5px;
+      }
+      #list-compact.desktop {
+        grid-template-columns: 60px 3fr 90px 2fr 29.5px;
       }
 
-      a {
+      #list-compact:hover #title {
+        text-decoration: underline;
+      }
+
+      #title {
         color: #4b64ff;
         text-decoration: none;
-      }
-
-      a:hover,
-      a:focus {
-        text-decoration: underline;
       }
 
       #title,
@@ -114,7 +106,8 @@ export class TileListCompact extends LitElement {
         white-space: nowrap;
       }
 
-      #views {
+      #views,
+      #date {
         text-align: right;
         color: #767676;
       }
