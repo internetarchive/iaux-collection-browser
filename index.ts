ab4cd5442ad4f23b7edc9f2380d0bc5a22c52905
Jason Buckner <jason@jasonbuckner.com>
2022-03-29 11:57:15 -0700

    Add some exports

--- index.ts
+++ index.ts
@@ -1,1 +1,3 @@
 export { CollectionBrowser } from './src/collection-browser';
+export { SortFilterBar } from './src/sort-filter-bar/sort-filter-bar';
+export { CollectionDisplayMode } from './src/models';
