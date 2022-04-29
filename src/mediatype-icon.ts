3619d9867c27097715b0666b8b33463886db25c0
Clau Yagyagan <1281581+dualcnhq@users.noreply.github.com>
2022-04-30 03:33:43 +0800

    Refactor mediatype data display

--- src/mediatype-icon.ts
+++ src/mediatype-icon.ts
@@ -2,21 +2,7 @@ import { css, CSSResultGroup, html, LitElement } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 import { ifDefined } from 'lit/directives/if-defined.js';
 
-import { accountIcon } from './assets/img/icons/mediatype/account';
-import { audioIcon } from './assets/img/icons/mediatype/audio';
-import { collectionIcon } from './assets/img/icons/mediatype/collection';
-import { dataIcon } from './assets/img/icons/mediatype/data';
-import { etreeIcon } from './assets/img/icons/mediatype/etree';
-import { imagesIcon } from './assets/img/icons/mediatype/images';
-import { filmIcon } from './assets/img/icons/mediatype/film';
-import { radioIcon } from './assets/img/icons/mediatype/radio';
-import { softwareIcon } from './assets/img/icons/mediatype/software';
-import { textsIcon } from './assets/img/icons/mediatype/texts';
-import { tvIcon } from './assets/img/icons/mediatype/tv';
-import { videoIcon } from './assets/img/icons/mediatype/video';
-import { webIcon } from './assets/img/icons/mediatype/web';
-
-import { mediatypeColor, mediatypeText } from './mediatype/mediatype-display';
+import { mediatypeData } from './mediatype/mediatype-display';
 
 @customElement('mediatype-icon')
 export class MediatypeIcon extends LitElement {
@@ -25,23 +11,6 @@ export class MediatypeIcon extends LitElement {
   @property({ type: Array }) collections: string[] | undefined;
 
   @property({ type: Boolean }) showText = false;
-
-  private readonly mediatypeIcons: { [key: string]: any } = {
-    account: accountIcon,
-    audio: audioIcon,
-    collection: collectionIcon,
-    data: dataIcon,
-    etree: etreeIcon,
-    film: filmIcon,
-    image: imagesIcon,
-    movies: filmIcon,
-    radio: radioIcon,
-    software: softwareIcon,
-    texts: textsIcon,
-    tv: tvIcon,
-    video: videoIcon,
-    web: webIcon,
-  };
 
   private get displayMediaType() {
     const tvIdentifier = ['tvnews', 'tvarchive', 'television'];
@@ -74,13 +43,15 @@ export class MediatypeIcon extends LitElement {
         id="icon"
         class="${this.showText ? 'show-text' : 'hide-text'}"
         style="--iconFillColor: ${ifDefined(
-          mediatypeColor[displayMediatype]
-            ? mediatypeColor[displayMediatype]
+          mediatypeData[displayMediatype]
+            ? mediatypeData[displayMediatype]?.color
             : undefined
         )}"
       >
-        ${ifDefined(this.mediatypeIcons[displayMediatype])}
-        <p class="status-text">${ifDefined(mediatypeText[displayMediatype])}</p>
+        ${ifDefined(mediatypeData[displayMediatype].icon)}
+        <p class="status-text">
+          ${ifDefined(mediatypeData[displayMediatype].text)}
+        </p>
       </div>
     `;
   }
