3d7218b0f1bbb2f70963823ba4ab2cb025073a60
Jason Buckner <jason@jasonbuckner.com>
2022-05-05 14:11:27 -0700

    Add some additional tests (#40)

---
+++ test/mocks/mock-search-service.ts
@@ -0,0 +1,35 @@
+import { Result } from '@internetarchive/result-type';
+import {
+  MetadataResponse,
+  SearchParams,
+  SearchResponse,
+  SearchServiceInterface,
+  SearchServiceError,
+} from '@internetarchive/search-service';
+import { mockSuccessResponse } from './mock-search-responses';
+
+export class MockSearchService implements SearchServiceInterface {
+  searchParams?: SearchParams;
+
+  async search(
+    params: SearchParams
+  ): Promise<Result<SearchResponse, SearchServiceError>> {
+    this.searchParams = params;
+    return mockSuccessResponse;
+  }
+
+  async fetchMetadata(
+    identifier: string
+  ): Promise<Result<MetadataResponse, SearchServiceError>> {
+    console.debug('fetchMetadata', identifier);
+    throw new Error('Method not implemented.');
+  }
+
+  async fetchMetadataValue<T>(
+    identifier: string,
+    keypath: string
+  ): Promise<Result<T, SearchServiceError>> {
+    console.debug('fetchMetadataValue', identifier, keypath);
+    throw new Error('Method not implemented.');
+  }
+}
