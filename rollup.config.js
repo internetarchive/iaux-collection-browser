28824a40c44f50f1bf21009a0006ea41c30fb1e9
Clau Yagyagan <1281581+dualcnhq@users.noreply.github.com>
2022-05-26 10:03:32 +0800

    Update rollup config and cleanup deps

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,35 @@
-// source from: https://open-wc.org/docs/building/rollup/
-import merge from 'deepmerge';
-import { createSpaConfig } from '@open-wc/building-rollup';
+// source: https://lit.dev/docs/tools/production/
 
-const baseConfig = createSpaConfig({
-  outputDir: 'dist',
-  developmentMode: process.env.ROLLUP_WATCH === 'true',
-  injectServiceWorker: false,
-});
+// Import rollup plugins
+import html from '@web/rollup-plugin-html';
+import {copy} from '@web/rollup-plugin-copy';
+import resolve from '@rollup/plugin-node-resolve';
+import {terser} from 'rollup-plugin-terser';
+import minifyHTML from 'rollup-plugin-minify-html-literals';
+import summary from 'rollup-plugin-summary';
 
-export default merge(baseConfig, {
-  input: 'demo/index.html',
-});
+export default {
+  plugins: [
+    // Entry point for application build; can specify a glob to build multiple
+    // HTML files for non-SPA app
+    html({
+      input: 'demo/index.html',
+    }),
+    // Resolve bare module specifiers to relative paths
+    resolve(),
+    // Minify HTML template literals
+    minifyHTML(),
+    // Minify JS
+    terser({
+      ecma: 2020,
+      module: true,
+      warnings: true,
+    }),
+    // Print bundle summary
+    summary(),
+  ],
+  output: {
+    dir: 'dist',
+  },
+  preserveEntrySignatures: 'strict',
+};
