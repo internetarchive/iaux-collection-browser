1d3f5e075c0517f5e2ee2b463d0c87bdc74cac64
Clau Yagyagan <1281581+dualcnhq@users.noreply.github.com>
2022-05-26 10:05:13 +0800

    Remove unused import

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,6 @@
 
 // Import rollup plugins
 import html from '@web/rollup-plugin-html';
-import {copy} from '@web/rollup-plugin-copy';
 import resolve from '@rollup/plugin-node-resolve';
 import {terser} from 'rollup-plugin-terser';
 import minifyHTML from 'rollup-plugin-minify-html-literals';
