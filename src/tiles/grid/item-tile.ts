96052ef29a05b77b359f4ab50e49a37255dd00b3
clau <1281581+dualcnhq@users.noreply.github.com>
2022-06-15 19:21:56 +0800

    Brought back styles in item-tile component

--- src/tiles/grid/item-tile.ts
+++ src/tiles/grid/item-tile.ts
@@ -1,5 +1,5 @@
 /* eslint-disable import/no-duplicates */
-import { CSSResultGroup, html, LitElement, nothing } from 'lit';
+import { css, CSSResultGroup, html, LitElement, nothing } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 import { ifDefined } from 'lit/directives/if-defined.js';
 
@@ -8,8 +8,6 @@ import { TileModel } from '../../models';
 import '../mediatype-icon';
 import '../item-image';
 import '../item-stats';
-
-import { itemTileStyles } from '../../styles/item-tile-styles';
 
 @customElement('item-tile')
 export class ItemTile extends LitElement {
@@ -54,6 +52,89 @@ export class ItemTile extends LitElement {
   }
 
   static get styles(): CSSResultGroup {
-    return [itemTileStyles];
+    return css`
+      #container {
+        background-color: #ffffff;
+        border-radius: var(--collectionTileCornerRadius, 4px);
+        box-shadow: 1px 1px 2px 0px;
+        display: flex;
+        flex-direction: column;
+        height: 100%;
+      }
+
+      .title-wrapper {
+        flex-shrink: 0;
+      }
+
+      .image-wrapper {
+        flex-grow: 1;
+      }
+
+      #item-title {
+        color: #2c2c2c;
+        font-size: 1.6rem;
+        text-align: center;
+        margin-top: 0rem;
+        margin-bottom: 0.5rem;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        display: -webkit-box;
+        -webkit-line-clamp: 2;
+        -webkit-box-orient: vertical;
+        line-height: 2rem;
+        height: 4rem;
+      }
+
+      #item-image-container {
+        display: flex;
+        justify-content: center;
+        flex: 1;
+        height: 16rem;
+      }
+
+      .hidden {
+        display: none;
+      }
+
+      #container:hover > #title-image-container > .item-title {
+        text-decoration: underline;
+      }
+
+      /** this is a workaround for Safari 15 where the hover effects are not working */
+      #title-image-container:hover > #item-title {
+        text-decoration: underline;
+      }
+
+      #container:hover > #item-title {
+        background-color: #fcfcfc;
+      }
+
+      .item-creator {
+        display: flex;
+        justify-content: center;
+        align-items: flex-end; /* Important to start text from bottom */
+        height: 3rem;
+        padding-top: 1rem;
+        margin-top: 5px;
+      }
+
+      .truncated {
+        flex: 1;
+        min-width: 0; /* Important for long words! */
+      }
+
+      .truncated span {
+        font-size: 1.4rem;
+        color: #2c2c2c;
+        -webkit-line-clamp: 2;
+        text-overflow: ellipsis;
+        overflow: hidden;
+        display: -webkit-box;
+        -webkit-box-orient: vertical;
+        word-wrap: break-word;
+        line-height: 2rem;
+        text-align: center;
+      }
+    `;
   }
 }
