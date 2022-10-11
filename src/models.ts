9169a5df988fbbc5f3e047b52c249a7ebc0c8b0c
Laton Vermette <1619661+latonv@users.noreply.github.com>
2022-10-11 13:20:08 -0700

    Add more items to list of admin collections

--- src/models.ts
+++ src/models.ts
@@ -216,10 +216,19 @@ export const lendingFacetDisplayNames: Partial<
 };
 
 /**
- * A record of which collections should be suppressed from being displayed
+ * A record of which admin-only collections should be suppressed from being displayed
  * as facets or in an item's list of collections.
  */
 export const suppressedCollections: Record<string, boolean> = {
   deemphasize: true,
   community: true,
+  stream_only: true,
+  samples_only: true,
+  test_collection: true,
+  printdisabled: true,
+  'openlibrary-ol': true,
+  nationalemergencylibrary: true,
+  china: true,
+  americana: true,
+  toronto: true,
 };
