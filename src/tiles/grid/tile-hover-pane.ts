a93cac9f68c965a69d2f687d1c832264f36414b9
Laton Vermette <1619661+latonv@users.noreply.github.com>
2022-11-24 14:39:57 -0800

    Construct basic subcomponent templates

--- src/tiles/grid/tile-hover-pane.ts
+++ src/tiles/grid/tile-hover-pane.ts
@@ -6,11 +6,54 @@ import type { TileModel } from '../../models';
 export class TileHoverPane extends LitElement {
   @property({ type: Object }) model?: TileModel;
 
+  @property({ type: String }) baseImageUrl?: string;
+
+  @property({ type: Boolean }) loggedIn?: boolean;
+
   protected render(): TemplateResult {
-    return html``;
+    return html`
+      <div class="container">
+        ${this.imageTemplate} ${this.itemDetailsTemplate}
+      </div>
+    `;
+  }
+
+  private get imageTemplate(): TemplateResult {
+    return html`
+      <div class="image-container">
+        <image-block
+          .model=${this.model}
+          .baseImageUrl=${this.baseImageUrl}
+          .isCompactTile=${false}
+          .isListTile=${true}
+          .viewSize=${'desktop'}
+          .loggedIn=${this.loggedIn}
+        ></image-block>
+      </div>
+    `;
+  }
+
+  private get itemDetailsTemplate(): TemplateResult {
+    return html` <div class="item-details"></div> `;
   }
 
   static get styles(): CSSResultGroup {
-    return css``;
+    return css`
+      .container {
+        display: flex;
+        padding: 10px;
+        border: 1px solid #ddd;
+        border-radius: 4px;
+        box-shadow: 4px 4px 8px 0 black;
+      }
+
+      .image-container {
+        flex-grow: 0;
+      }
+
+      .item-details {
+        flex-grow: 1;
+      }
+    `;
   }
 }
