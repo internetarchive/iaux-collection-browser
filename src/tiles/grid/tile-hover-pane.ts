f353a8c5b9353324c53297a3ba6aed76ceccc9e1
Laton Vermette <1619661+latonv@users.noreply.github.com>
2022-11-25 14:43:29 -0800

    Add nav url to hover pane

--- src/tiles/grid/tile-hover-pane.ts
+++ src/tiles/grid/tile-hover-pane.ts
@@ -3,10 +3,13 @@ import type { SortParam } from '@internetarchive/search-service';
 import { css, CSSResultGroup, html, LitElement, TemplateResult } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 import type { TileModel } from '../../models';
+import '../list/tile-list';
 
 @customElement('tile-hover-pane')
 export class TileHoverPane extends LitElement {
   @property({ type: Object }) model?: TileModel;
+
+  @property({ type: String }) baseNavigationUrl?: string;
 
   @property({ type: String }) baseImageUrl?: string;
 
@@ -19,9 +22,10 @@ export class TileHoverPane extends LitElement {
 
   protected render(): TemplateResult {
     return html`
-      <div class="container">
+      <div id="container">
         <tile-list
           .model=${this.model}
+          .baseNavigationUrl=${this.baseNavigationUrl}
           .baseImageUrl=${this.baseImageUrl}
           .loggedIn=${this.loggedIn}
           .sortParam=${this.sortParam}
@@ -33,8 +37,9 @@ export class TileHoverPane extends LitElement {
 
   static get styles(): CSSResultGroup {
     return css`
-      .container {
-        max-width: 50vw;
+      #container {
+        width: 700px;
+        max-width: 45vw;
         padding: 10px;
         border: 1px solid #ddd;
         border-radius: 4px;
