89f38d84f819ef7f70b9ca7ad0a88a926676e9d3
Neeraj Sharma <50868725+nsharma123@users.noreply.github.com>
2022-11-30 13:41:24 +0530

    Revised Patron Tile (UI) with flexy height (#173)
    Revised Patron Tile (UI) with flexy height - webdev-5625

--- src/tiles/grid/account-tile.ts
+++ src/tiles/grid/account-tile.ts
@@ -1,123 +1,75 @@
-import { css, html, LitElement } from 'lit';
+import { css, html, LitElement, TemplateResult } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 import type { TileModel } from '../../models';
 
+import { baseTileStyles } from './styles/tile-grid-shared-styles';
+import '../image-block';
 import './tile-stats';
 
 @customElement('account-tile')
 export class AccountTile extends LitElement {
   @property({ type: Object }) model?: TileModel;
 
+  @property({ type: String }) baseImageUrl?: string;
+
   render() {
     return html`
-      <div class="account-tile-main">
-        <div id="title">
-          <h1 class="truncated">${this.model?.identifier}</h1>
-        </div>
-
-        <div class="account-info">
-          <div id="avatar-info">
-            <img
-              id="avatar"
-              alt="patron-avatar"
-              src="https://archive.org/services/img/${this.model?.identifier}"
-            />
+      <div class="container">
+        <div class="tile-details">
+          <div class="item-info">
+            ${this.getAvatarTemplate} ${this.getTitleTemplate}
+            ${this.getArchivistTemplate}
           </div>
-
-          <span id="archivist-since">
-            Archivist since ${this.model?.dateAdded?.getFullYear()}
-          </span>
+          ${this.getTileStatsTemplate}
         </div>
-
-        <tile-stats
-          .mediatype=${this.model?.mediatype}
-          .itemCount=${this.model?.itemCount}
-          .favCount=${this.model?.favCount}
-          .commentCount=${this.model?.commentCount}
-        >
-        </tile-stats>
       </div>
     `;
   }
 
-  static get styles() {
-    return css`
-      h1 {
-        color: black;
-        font-size: 16px;
-        margin: 0;
-      }
-
-      span {
-        font-size: 14px;
-        color: #2c2c2c;
-        margin: 0px;
-      }
-
-      .account-tile-main {
-        background-color: #fcf5e6;
-        border: 1px #2c2c2c;
-        border-radius: 4px;
-        box-shadow: 1px 1px 2px 0px;
-        height: 100%;
-        display: flex;
-        flex-direction: column;
-        text-align: center;
-        width: 100%;
-      }
-
-      .account-info {
-        flex-grow: 1;
-      }
-
-      #title {
-        padding: 5px 5px 0px 5px;
-        flex-shrink: 0;
-        height: 40px;
-      }
-
-      .account-tile-main:hover > #title > .truncated {
-        text-decoration: underline;
-      }
-
-      /** this is a workaround for Safari 15 where the hover effects are not working */
-      #title:hover > .truncated {
-        text-decoration: underline;
-      }
-
-      #avatar-info {
-        margin-top: 5px;
-        margin-bottom: 5px;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-      }
-
-      #avatar {
-        background-position: 50% 50%;
-        border-radius: 50%;
-        width: 160px;
-        height: 160px;
-        box-shadow: 1px 1px 2px #888888;
-      }
-
-      #archivist-since {
-        margin-bottom: 5px;
-        height: 40px;
-      }
-
-      .truncated {
-        flex: 1;
-        min-width: 0; /* Important for long words! */
-        -webkit-line-clamp: 2;
-        text-overflow: ellipsis;
-        overflow: hidden;
-        display: -webkit-box;
-        -webkit-box-orient: vertical;
-        word-wrap: break-word;
-        line-height: 2rem;
-        text-align: center;
-      }
+  private get getAvatarTemplate(): TemplateResult {
+    return html`
+      <image-block
+        .model=${this.model}
+        .baseImageUrl=${this.baseImageUrl}
+        .viewSize=${'grid'}
+      >
+      </image-block>
     `;
+  }
+
+  private get getTitleTemplate() {
+    return html`<div id="title">
+      <h1 class="truncated">${this.model?.identifier}</h1>
+    </div>`;
+  }
+
+  private get getArchivistTemplate() {
+    return html`<div class="archivist-since">
+      <span>Archivist since ${this.model?.dateAdded?.getFullYear()}</span>
+    </div>`;
+  }
+
+  private get getTileStatsTemplate() {
+    return html`<tile-stats
+      .mediatype=${this.model?.mediatype}
+      .itemCount=${this.model?.itemCount}
+      .favCount=${this.model?.favCount}
+      .commentCount=${this.model?.commentCount}
+    >
+    </tile-stats>`;
+  }
+
+  /**
+   * CSS
+   */
+  static get styles() {
+    return [
+      baseTileStyles,
+      css`
+        image-block {
+          --imageBlockBackgroundColor: #fcf5e6;
+        }
+      `,
+    ];
   }
 }
---
+++ test/tiles/grid/account-tile.test.ts
@@ -0,0 +1,95 @@
+/* eslint-disable import/no-duplicates */
+import { expect, fixture } from '@open-wc/testing';
+import { html } from 'lit';
+import type { AccountTile } from '../../../src/tiles/grid/account-tile';
+
+import '../../../src/tiles/grid/account-tile';
+
+describe('Account Tile', () => {
+  it('should render initial component', async () => {
+    const el = await fixture<AccountTile>(html`<account-tile></account-tile>`);
+
+    const itemInfo = el.shadowRoot?.querySelector('.item-info');
+    const itemImage = el.shadowRoot?.querySelector('image-block');
+    const itemArchivist = el.shadowRoot?.querySelector('.archivist-since');
+    const tileStats = el.shadowRoot?.querySelector('tile-stats');
+
+    expect(itemInfo).to.exist;
+    expect(itemImage).to.exist;
+    expect(itemArchivist).to.exist;
+    expect(tileStats).to.exist;
+  });
+
+  it('should render with title element', async () => {
+    const el = await fixture<AccountTile>(html`
+      <account-tile
+        .model=${{
+          identifier: '@jack-sparrow',
+        }}
+      >
+      </account-tile>
+    `);
+    await el.updateComplete;
+
+    const itemInfo = el.shadowRoot?.querySelector('.item-info');
+    const accountTitle = el.shadowRoot?.querySelector('#title');
+
+    expect(itemInfo).to.exist;
+    expect(accountTitle).to.exist;
+    expect(accountTitle?.querySelector('.truncated')?.textContent).to.equal(
+      '@jack-sparrow'
+    );
+  });
+
+  it('should render with archivist-since date element', async () => {
+    const el = await fixture<AccountTile>(html`
+      <account-tile
+        .model=${{
+          dateAdded: new Date('2022-01-01'),
+        }}
+      >
+      </account-tile>
+    `);
+    await el.updateComplete;
+
+    const itemArchivist = el.shadowRoot?.querySelector('.archivist-since');
+    expect(itemArchivist?.textContent).contains('Archivist since 2022');
+  });
+
+  it('should render with tile-stats element', async () => {
+    const el = await fixture<AccountTile>(html`
+      <account-tile
+        .model=${{
+          identifier: '@jack-sparrow',
+          mediatype: 'account',
+          itemCount: 999,
+          favCount: 100,
+          commentCount: 3,
+        }}
+      >
+      </account-tile>
+    `);
+    await el.updateComplete;
+
+    const tileStats = el.shadowRoot?.querySelector('tile-stats');
+    const statsElements = tileStats?.shadowRoot?.querySelector('#stats-row');
+
+    expect(tileStats).to.exist;
+    expect(statsElements).to.exist;
+
+    const downloads = statsElements
+      ?.querySelectorAll('.col')[1]
+      .querySelector('.status-text');
+    expect(downloads?.textContent).contains(999);
+
+    const favorites = statsElements
+      ?.querySelectorAll('.col')[2]
+      .querySelector('.status-text');
+    expect(favorites?.textContent).contains(100);
+
+    const reviewCounts = statsElements
+      ?.querySelectorAll('.col')[3]
+      .querySelector('.status-text');
+    expect(reviewCounts?.textContent).contains(3);
+  });
+});
