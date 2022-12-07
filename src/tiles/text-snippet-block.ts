d4df8551c8445109ae4ee6b61ae31fc3f0e44d82
clau <1281581+dualcnhq@users.noreply.github.com>
2022-12-08 06:58:13 +0800

    Change default container top margin value

--- src/tiles/text-snippet-block.ts
+++ src/tiles/text-snippet-block.ts
@@ -87,7 +87,7 @@ export class TextSnippetBlock extends LitElement {
         flex-wrap: wrap;
         width: 100%;
         border-left: 5px solid #194880;
-        margin-top: var(--containerTopMargin, 5px);
+        margin-top: var(--containerTopMargin, 10px);
         margin-left: var(--containerLeftMargin, 0px);
         border-radius: 3px;
       }
