159dcfef7466a1f3e1dd9bd062df477949e8074c
latonv <1619661+latonv@users.noreply.github.com>
2022-12-15 11:38:11 -0800

    WEBDEV-5680 Ensure title/creator letter filters clear when mobile sort field changes (#185)
    * Ensure title/creator letter filters are cleared when mobile sort field changed
    
    * Ensure correct alpha bar type is rendered after title/creator filter cleared
    
    * Fix incorrect logic around alpha selector visibility
    
    * Add tests for mobile view sort/filter interactions

--- src/sort-filter-bar/sort-filter-bar.ts
+++ src/sort-filter-bar/sort-filter-bar.ts
@@ -380,7 +380,18 @@ export class SortFilterBar
 
   private mobileSortChanged(e: Event) {
     const target = e.target as HTMLSelectElement;
-    this.setSelectedSort(target.value as SortField);
+    const sortField = target.value as SortField;
+    this.setSelectedSort(sortField);
+
+    this.alphaSelectorVisible = null;
+    if (sortField !== 'title' && this.selectedTitleFilter) {
+      this.selectedTitleFilter = null;
+      this.emitTitleLetterChangedEvent();
+    }
+    if (sortField !== 'creator' && this.selectedCreatorFilter) {
+      this.selectedCreatorFilter = null;
+      this.emitCreatorLetterChangedEvent();
+    }
   }
 
   private get displayOptionTemplate() {
