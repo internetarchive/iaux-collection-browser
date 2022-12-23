2618a758193542169c74b83bf7311ed4b8ccc9cc
dualcnhq <1281581+dualcnhq@users.noreply.github.com>
2022-12-23 20:49:24 +0000

    Deploying to gh-pages from @ internetarchive/iaux-collection-browser@f0a7c2aec2e1db619c9ee3a645ef43643118244d 🚀

--- main/assets/main.a825f334.js
+++ main/assets/main.a825f334.js
@@ -3929,6 +3929,7 @@ In order to be iterable, non-array objects must have a [Symbol.iterator]() metho
       min-width: 18rem; /* Prevents Safari from shrinking col at first draw */
       padding-right: 12px;
       padding-right: 2.5rem;
+      z-index: 1;
     }
 
     .desktop #left-column::-webkit-scrollbar {
