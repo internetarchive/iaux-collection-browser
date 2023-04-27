ae16134a3971333ee163973b2baf9dfae62448b8
Laton Vermette <1619661+latonv@users.noreply.github.com>
2023-04-26 22:45:57 -0700

    Add SHA-1 util function

---
+++ src/utils/sha1.ts
@@ -0,0 +1,9 @@
+const encoder = new TextEncoder();
+
+/** Returns (asynchronously) the hex-encoded SHA-1 hash of the input string */
+export async function sha1(input: string): Promise<string> {
+  const hashBuffer = await crypto.subtle.digest('sha-1', encoder.encode(input));
+  return [...new Uint8Array(hashBuffer)]
+    .map(byte => byte.toString(16).padStart(2, '0'))
+    .join('');
+}
