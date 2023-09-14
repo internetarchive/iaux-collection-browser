1e63d0a01f81fe4dba69542ec257f60a36dab1b5
Laton Vermette <1619661+latonv@users.noreply.github.com>
2023-09-14 13:04:58 -0700

    Add add'l tests for facet template events

--- test/collection-facets/facets-template.test.ts
+++ test/collection-facets/facets-template.test.ts
@@ -1,4 +1,5 @@
 import { expect, fixture } from '@open-wc/testing';
+import sinon from 'sinon';
 import { html } from 'lit';
 import type { FacetsTemplate } from '../../src/collection-facets/facets-template';
 import '../../src/collection-facets/facets-template';
@@ -33,7 +34,6 @@ describe('Render facets', () => {
         .renderOn=${'page'}
       ></facets-template>`
     );
-    await el.updateComplete;
 
     expect(el.shadowRoot?.querySelector('.facets-on-page')).to.exist;
   });
@@ -45,7 +45,6 @@ describe('Render facets', () => {
         .renderOn=${'modal'}
       ></facets-template>`
     );
-    await el.updateComplete;
 
     expect(el.shadowRoot?.querySelector('.facets-on-modal')).to.exist;
   });
@@ -54,7 +53,6 @@ describe('Render facets', () => {
     const el = await fixture<FacetsTemplate>(
       html`<facets-template .facetGroup=${facetGroup}></facets-template>`
     );
-    await el.updateComplete;
 
     const facetRows = el.shadowRoot?.querySelectorAll('facet-row');
     expect(facetRows?.length).to.equal(facetGroup.buckets.length);
@@ -65,5 +63,65 @@ describe('Render facets', () => {
       expect(facetRow.bucket).to.equal(facetGroup.buckets[i]);
       expect(facetRow.facetType).to.equal(facetGroup.key);
     });
+  });
+
+  it('emits facet click and change events when a facet is selected/deselected', async () => {
+    const facetClickSpy = sinon.spy();
+    const facetsChangeSpy = sinon.spy();
+    const el = await fixture<FacetsTemplate>(
+      html`<facets-template
+        .facetGroup=${facetGroup}
+        @facetClick=${facetClickSpy}
+        @selectedFacetsChanged=${facetsChangeSpy}
+      ></facets-template>`
+    );
+
+    const facetRow = el.shadowRoot?.querySelector('facet-row') as FacetRow;
+    expect(facetRow).to.exist;
+
+    const facetSelectCheck = facetRow.shadowRoot?.querySelector(
+      '.select-facet-checkbox'
+    ) as HTMLInputElement;
+    expect(facetSelectCheck).to.exist;
+
+    facetSelectCheck.click();
+    expect(facetClickSpy.callCount).to.equal(1);
+    expect(facetsChangeSpy.callCount).to.equal(1);
+    expect(facetRow.bucket?.state).to.equal('selected');
+
+    facetSelectCheck.click();
+    expect(facetClickSpy.callCount).to.equal(2);
+    expect(facetsChangeSpy.callCount).to.equal(2);
+    expect(facetRow.bucket?.state).to.equal('none');
+  });
+
+  it('emits facet click and change events when a facet is negated/un-negated', async () => {
+    const facetClickSpy = sinon.spy();
+    const facetsChangeSpy = sinon.spy();
+    const el = await fixture<FacetsTemplate>(
+      html`<facets-template
+        .facetGroup=${facetGroup}
+        @facetClick=${facetClickSpy}
+        @selectedFacetsChanged=${facetsChangeSpy}
+      ></facets-template>`
+    );
+
+    const facetRow = el.shadowRoot?.querySelector('facet-row') as FacetRow;
+    expect(facetRow).to.exist;
+
+    const facetNegateCheck = facetRow.shadowRoot?.querySelector(
+      '.hide-facet-checkbox'
+    ) as HTMLInputElement;
+    expect(facetNegateCheck).to.exist;
+
+    facetNegateCheck.click();
+    expect(facetClickSpy.callCount).to.equal(1);
+    expect(facetsChangeSpy.callCount).to.equal(1);
+    expect(facetRow.bucket?.state).to.equal('hidden');
+
+    facetNegateCheck.click();
+    expect(facetClickSpy.callCount).to.equal(2);
+    expect(facetsChangeSpy.callCount).to.equal(2);
+    expect(facetRow.bucket?.state).to.equal('none');
   });
 });
