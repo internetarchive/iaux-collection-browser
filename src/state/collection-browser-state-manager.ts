2c07607b47a7603be8e657f892389fe061938b3f
Laton Vermette <1619661+latonv@users.noreply.github.com>
2023-10-24 14:57:44 -0700

    Remove obsolete state management stub

--- src/state/collection-browser-state-manager.ts
+++
@@ -1,9 +0,0 @@
-import { getDefaultSelectedFacets } from '../models';
-import type { CollectionBrowserState } from './models';
-
-export class CollectionBrowserStateManager {
-  private _state: CollectionBrowserState = {
-    baseQuery: '',
-    selectedFacets: getDefaultSelectedFacets(),
-  };
-}
