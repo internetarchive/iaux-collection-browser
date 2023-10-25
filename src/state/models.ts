2f5a844469681599c5fcdca152c857f47e309afd
Laton Vermette <1619661+latonv@users.noreply.github.com>
2023-10-25 12:48:41 -0700

    Build out some more robust interfaces for state management

--- src/state/models.ts
+++ src/state/models.ts
@@ -1,20 +1,18 @@
-import type { SortDirection } from '@internetarchive/search-service';
+import type {
+  SearchType,
+  SortDirection,
+} from '@internetarchive/search-service';
 import type { SelectedFacets, SortField, TileModel } from '../models';
-
-export class SortOption {
-  // eslint-disable-next-line no-useless-constructor
-  constructor(readonly field: SortField, readonly direction: SortDirection) {
-    // No body, just defining properties via args. eslint likes to complain about this.
-  }
-}
 
 export interface CollectionBrowserState {
   baseQuery: string;
   withinCollection?: string;
+  searchType: SearchType;
   selectedFacets: SelectedFacets;
   minSelectedYear?: string;
   maxSelectedYear?: string;
-  sort?: SortOption;
+  selectedSort?: SortField;
+  sortDirection?: SortDirection;
   selectedTitlePrefix?: string;
   selectedCreatorPrefix?: string;
 }
