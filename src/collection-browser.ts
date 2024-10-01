256796746aa2b2cb418ca90192e0b546c614eab8
Laton Vermette <1619661+latonv@users.noreply.github.com>
2024-10-01 13:15:06 -0700

    Add additional logging

--- src/collection-browser.ts
+++ src/collection-browser.ts
@@ -1192,6 +1192,11 @@ export class CollectionBrowser
     this.dataSourceInstallInProgress = false;
 
     if (!this.searchResultsLoading) {
+      console.log(
+        'setting total result count and tile count from installed data source',
+        this.dataSource.totalResults,
+        this.dataSource.size
+      );
       this.setTotalResultCount(this.dataSource.totalResults);
       this.setTileCount(this.dataSource.size);
     }
@@ -1367,7 +1372,12 @@ export class CollectionBrowser
     }
 
     if (changed.has('pagesToRender')) {
+      console.log('pagesToRender changed', this.pagesToRender);
       if (!this.dataSource.endOfDataReached && this.infiniteScroller) {
+        console.log(
+          'updating infinite scroller item count to new estimate',
+          this.estimatedTileCount
+        );
         this.infiniteScroller.itemCount = this.estimatedTileCount;
       }
     }
@@ -1679,6 +1689,10 @@ export class CollectionBrowser
     // Reset the infinite scroller's item count, so that it
     // shows tile placeholders until the new query's results load in
     if (this.infiniteScroller) {
+      console.log(
+        'resetting infinite scroller tile count to estimate',
+        this.estimatedTileCount
+      );
       this.infiniteScroller.itemCount = this.estimatedTileCount;
       this.infiniteScroller.reload();
     }
@@ -1838,6 +1852,7 @@ export class CollectionBrowser
    * Sets the total number of tiles displayed in the infinite scroller.
    */
   setTileCount(count: number): void {
+    console.log('setTileCount called', count, !!this.infiniteScroller);
     if (this.infiniteScroller) {
       this.infiniteScroller.itemCount = count;
     }
