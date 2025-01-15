949de04e1ef6382a1b3a54c64f127bbab40e26eb
Laton Vermette <1619661+latonv@users.noreply.github.com>
2025-01-15 14:23:20 -0800

    Fix discrepancy affecting old year-only publish dates

--- test/utils/format-date.test.ts
+++ test/utils/format-date.test.ts
@@ -16,6 +16,23 @@ describe('formatDate', () => {
     expect(formatDate(testDate, 'long')).to.equal('Dec 09, 2020');
   });
 
+  it('returns year-only date when year-only DateFormat', () => {
+    expect(formatDate(testDate, 'year-only')).to.equal(2020);
+  });
+
+  it('returns correct year for old "Jan 1 at midnight" dates and year-only DateFormat', () => {
+    // Many standard timezones have a discontinuity in date parsing at some point during
+    // the 19th or 20th century, corresponding to the creation of the timezone.
+    // Dates prior to the discontinuity generally have a non-hour-aligned timezone offset
+    // which can throw off the calculated year for dates which are close to a year boundary.
+    // This is particularly problematic for "Jan 1 at midnight" dates, which are what we
+    // receive from the search engine for date metadata that only specifies the year.
+    // So we must ensure these older dates still output the correct year, not the prior one.
+    expect(formatDate(new Date('1234-01-01T00:00:00Z'), 'year-only')).to.equal(
+      1234
+    );
+  });
+
   it('returns locale formatted date', () => {
     expect(formatDate(testDate, 'long', 'de-DE')).to.equal('09. Dez. 2020');
   });
