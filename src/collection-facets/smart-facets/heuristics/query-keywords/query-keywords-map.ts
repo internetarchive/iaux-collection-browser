3e75be5657f1e631220416398ff191d5a0ce37d0
Laton Vermette <1619661+latonv@users.noreply.github.com>
2025-03-14 13:50:36 -0700

    Reorganize heuristics

---
+++ src/collection-facets/smart-facets/heuristics/query-keywords/query-keywords-map.ts
@@ -0,0 +1,36 @@
+import { KeywordFacetMap } from '../../models';
+
+/**
+ * Map from keywords found in the search query to an array of
+ * likely-relevant "smart facets" for those keywords.
+ */
+export const QUERY_KEYWORDS: Readonly<KeywordFacetMap> = {
+  text: [{ facets: [{ facetType: 'mediatype', bucketKey: 'texts' }] }],
+  book: [{ facets: [{ facetType: 'mediatype', bucketKey: 'texts' }] }],
+  pdf: [{ facets: [{ facetType: 'mediatype', bucketKey: 'texts' }] }],
+  epub: [{ facets: [{ facetType: 'mediatype', bucketKey: 'texts' }] }],
+  audio: [{ facets: [{ facetType: 'mediatype', bucketKey: 'audio' }] }],
+  song: [{ facets: [{ facetType: 'mediatype', bucketKey: 'audio' }] }],
+  music: [{ facets: [{ facetType: 'mediatype', bucketKey: 'audio' }] }],
+  listen: [{ facets: [{ facetType: 'mediatype', bucketKey: 'audio' }] }],
+  podcast: [{ facets: [{ facetType: 'mediatype', bucketKey: 'audio' }] }],
+  radio: [{ facets: [{ facetType: 'mediatype', bucketKey: 'audio' }] }],
+  stream: [
+    { facets: [{ facetType: 'mediatype', bucketKey: 'audio' }] },
+    { facets: [{ facetType: 'mediatype', bucketKey: 'movies' }] },
+  ],
+  video: [{ facets: [{ facetType: 'mediatype', bucketKey: 'movies' }] }],
+  movie: [{ facets: [{ facetType: 'mediatype', bucketKey: 'movies' }] }],
+  film: [{ facets: [{ facetType: 'mediatype', bucketKey: 'movies' }] }],
+  image: [{ facets: [{ facetType: 'mediatype', bucketKey: 'image' }] }],
+  photo: [{ facets: [{ facetType: 'mediatype', bucketKey: 'image' }] }],
+  picture: [{ facets: [{ facetType: 'mediatype', bucketKey: 'image' }] }],
+  software: [{ facets: [{ facetType: 'mediatype', bucketKey: 'software' }] }],
+  app: [{ facets: [{ facetType: 'mediatype', bucketKey: 'software' }] }],
+  program: [{ facets: [{ facetType: 'mediatype', bucketKey: 'software' }] }],
+  game: [{ facets: [{ facetType: 'mediatype', bucketKey: 'software' }] }],
+  etree: [{ facets: [{ facetType: 'mediatype', bucketKey: 'etree' }] }],
+  concert: [{ facets: [{ facetType: 'mediatype', bucketKey: 'etree' }] }],
+  'live music': [{ facets: [{ facetType: 'mediatype', bucketKey: 'etree' }] }],
+  dataset: [{ facets: [{ facetType: 'mediatype', bucketKey: 'data' }] }],
+};
---
+++ src/collection-facets/smart-facets/heuristics/wikidata/wikidata-entity-map.ts
@@ -0,0 +1,134 @@
+import { KeywordFacetMap } from '../../models';
+
+/**
+ * Map from Wikidata description keywords for a given entity to a
+ * set of likely-relevant "smart facets" for that query.
+ *
+ * The placeholder string "__QUERY" should be substituted for the actual
+ * queried entity when generating facet values.
+ */
+export const WIKIDATA_ENTITIES: Readonly<KeywordFacetMap> = {
+  'written work': [
+    { facets: [{ facetType: 'mediatype', bucketKey: 'texts' }] },
+  ],
+  literature: [{ facets: [{ facetType: 'mediatype', bucketKey: 'texts' }] }],
+  book: [{ facets: [{ facetType: 'mediatype', bucketKey: 'texts' }] }],
+  novel: [{ facets: [{ facetType: 'mediatype', bucketKey: 'texts' }] }],
+  filmmaker: [
+    {
+      label: 'Films by __QUERY',
+      facets: [
+        { facetType: 'mediatype', bucketKey: 'movies' },
+        { facetType: 'creator', bucketKey: '__QUERY' },
+      ],
+    },
+  ],
+  author: [
+    {
+      label: 'Writing by __QUERY',
+      facets: [
+        { facetType: 'mediatype', bucketKey: 'texts' },
+        { facetType: 'creator', bucketKey: '__QUERY' },
+      ],
+    },
+  ],
+  writer: [
+    {
+      label: 'Writing by __QUERY',
+      facets: [
+        { facetType: 'mediatype', bucketKey: 'texts' },
+        { facetType: 'creator', bucketKey: '__QUERY' },
+      ],
+    },
+  ],
+  poet: [
+    {
+      label: 'Writing by __QUERY',
+      facets: [
+        { facetType: 'mediatype', bucketKey: 'texts' },
+        { facetType: 'creator', bucketKey: '__QUERY' },
+      ],
+    },
+  ],
+  photographer: [
+    {
+      label: 'Images by __QUERY',
+      facets: [
+        { facetType: 'mediatype', bucketKey: 'image' },
+        { facetType: 'creator', bucketKey: '__QUERY' },
+      ],
+    },
+  ],
+  painter: [
+    {
+      label: 'Images by __QUERY',
+      facets: [
+        { facetType: 'mediatype', bucketKey: 'image' },
+        { facetType: 'creator', bucketKey: '__QUERY' },
+      ],
+    },
+  ],
+  'visual artist': [
+    {
+      label: 'Images by __QUERY',
+      facets: [
+        { facetType: 'mediatype', bucketKey: 'image' },
+        { facetType: 'creator', bucketKey: '__QUERY' },
+      ],
+    },
+  ],
+  'graphic artist': [
+    {
+      label: 'Images by __QUERY',
+      facets: [
+        { facetType: 'mediatype', bucketKey: 'image' },
+        { facetType: 'creator', bucketKey: '__QUERY' },
+      ],
+    },
+  ],
+  singer: [
+    {
+      label: 'Music by __QUERY',
+      facets: [
+        { facetType: 'mediatype', bucketKey: 'audio' },
+        { facetType: 'creator', bucketKey: '__QUERY' },
+      ],
+    },
+  ],
+  songwriter: [
+    {
+      label: 'Music by __QUERY',
+      facets: [
+        { facetType: 'mediatype', bucketKey: 'audio' },
+        { facetType: 'creator', bucketKey: '__QUERY' },
+      ],
+    },
+  ],
+  musician: [
+    {
+      label: 'Music by __QUERY',
+      facets: [
+        { facetType: 'mediatype', bucketKey: 'audio' },
+        { facetType: 'creator', bucketKey: '__QUERY' },
+      ],
+    },
+  ],
+  composer: [
+    {
+      label: 'Music by __QUERY',
+      facets: [
+        { facetType: 'mediatype', bucketKey: 'audio' },
+        { facetType: 'creator', bucketKey: '__QUERY' },
+      ],
+    },
+  ],
+  pianist: [
+    {
+      label: 'Music by __QUERY',
+      facets: [
+        { facetType: 'mediatype', bucketKey: 'audio' },
+        { facetType: 'creator', bucketKey: '__QUERY' },
+      ],
+    },
+  ],
+};
