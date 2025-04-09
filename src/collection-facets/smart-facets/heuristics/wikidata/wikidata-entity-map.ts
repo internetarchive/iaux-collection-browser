964a90f8aee7eaacd9a047c7a55bbd8a80418eed
Laton Vermette <1619661+latonv@users.noreply.github.com>
2025-04-09 15:04:56 -0700

    Better organized wikidata entity map

--- src/collection-facets/smart-facets/heuristics/wikidata/wikidata-entity-map.ts
+++ src/collection-facets/smart-facets/heuristics/wikidata/wikidata-entity-map.ts
@@ -1,4 +1,48 @@
-import type { KeywordFacetMap } from '../../models';
+import type { KeywordFacetMap, SmartFacet } from '../../models';
+
+const WRITTEN_WORK_SMART_FACETS: SmartFacet[] = [
+  { facets: [{ facetType: 'mediatype', bucketKey: 'texts' }] },
+];
+
+const FILMMAKER_SMART_FACETS: SmartFacet[] = [
+  {
+    label: 'Films by __QUERY',
+    facets: [
+      { facetType: 'mediatype', bucketKey: 'movies' },
+      { facetType: 'creator', bucketKey: '__QUERY' },
+    ],
+  },
+];
+
+const AUTHOR_SMART_FACETS: SmartFacet[] = [
+  {
+    label: 'Writing by __QUERY',
+    facets: [
+      { facetType: 'mediatype', bucketKey: 'texts' },
+      { facetType: 'creator', bucketKey: '__QUERY' },
+    ],
+  },
+];
+
+const VISUAL_ARTIST_SMART_FACETS: SmartFacet[] = [
+  {
+    label: 'Images by __QUERY',
+    facets: [
+      { facetType: 'mediatype', bucketKey: 'image' },
+      { facetType: 'creator', bucketKey: '__QUERY' },
+    ],
+  },
+];
+
+const MUSICIAN_SMART_FACETS: SmartFacet[] = [
+  {
+    label: 'Music by __QUERY',
+    facets: [
+      { facetType: 'mediatype', bucketKey: 'audio' },
+      { facetType: 'creator', bucketKey: '__QUERY' },
+    ],
+  },
+];
 
 /**
  * Map from Wikidata description keywords for a given entity to a
@@ -8,127 +52,28 @@ import type { KeywordFacetMap } from '../../models';
  * queried entity when generating facet values.
  */
 export const WIKIDATA_ENTITIES: Readonly<KeywordFacetMap> = {
-  'written work': [
-    { facets: [{ facetType: 'mediatype', bucketKey: 'texts' }] },
-  ],
-  literature: [{ facets: [{ facetType: 'mediatype', bucketKey: 'texts' }] }],
-  book: [{ facets: [{ facetType: 'mediatype', bucketKey: 'texts' }] }],
-  novel: [{ facets: [{ facetType: 'mediatype', bucketKey: 'texts' }] }],
-  filmmaker: [
-    {
-      label: 'Films by __QUERY',
-      facets: [
-        { facetType: 'mediatype', bucketKey: 'movies' },
-        { facetType: 'creator', bucketKey: '__QUERY' },
-      ],
-    },
-  ],
-  author: [
-    {
-      label: 'Writing by __QUERY',
-      facets: [
-        { facetType: 'mediatype', bucketKey: 'texts' },
-        { facetType: 'creator', bucketKey: '__QUERY' },
-      ],
-    },
-  ],
-  writer: [
-    {
-      label: 'Writing by __QUERY',
-      facets: [
-        { facetType: 'mediatype', bucketKey: 'texts' },
-        { facetType: 'creator', bucketKey: '__QUERY' },
-      ],
-    },
-  ],
-  poet: [
-    {
-      label: 'Writing by __QUERY',
-      facets: [
-        { facetType: 'mediatype', bucketKey: 'texts' },
-        { facetType: 'creator', bucketKey: '__QUERY' },
-      ],
-    },
-  ],
-  photographer: [
-    {
-      label: 'Images by __QUERY',
-      facets: [
-        { facetType: 'mediatype', bucketKey: 'image' },
-        { facetType: 'creator', bucketKey: '__QUERY' },
-      ],
-    },
-  ],
-  painter: [
-    {
-      label: 'Images by __QUERY',
-      facets: [
-        { facetType: 'mediatype', bucketKey: 'image' },
-        { facetType: 'creator', bucketKey: '__QUERY' },
-      ],
-    },
-  ],
-  'visual artist': [
-    {
-      label: 'Images by __QUERY',
-      facets: [
-        { facetType: 'mediatype', bucketKey: 'image' },
-        { facetType: 'creator', bucketKey: '__QUERY' },
-      ],
-    },
-  ],
-  'graphic artist': [
-    {
-      label: 'Images by __QUERY',
-      facets: [
-        { facetType: 'mediatype', bucketKey: 'image' },
-        { facetType: 'creator', bucketKey: '__QUERY' },
-      ],
-    },
-  ],
-  singer: [
-    {
-      label: 'Music by __QUERY',
-      facets: [
-        { facetType: 'mediatype', bucketKey: 'audio' },
-        { facetType: 'creator', bucketKey: '__QUERY' },
-      ],
-    },
-  ],
-  songwriter: [
-    {
-      label: 'Music by __QUERY',
-      facets: [
-        { facetType: 'mediatype', bucketKey: 'audio' },
-        { facetType: 'creator', bucketKey: '__QUERY' },
-      ],
-    },
-  ],
-  musician: [
-    {
-      label: 'Music by __QUERY',
-      facets: [
-        { facetType: 'mediatype', bucketKey: 'audio' },
-        { facetType: 'creator', bucketKey: '__QUERY' },
-      ],
-    },
-  ],
-  composer: [
-    {
-      label: 'Music by __QUERY',
-      facets: [
-        { facetType: 'mediatype', bucketKey: 'audio' },
-        { facetType: 'creator', bucketKey: '__QUERY' },
-      ],
-    },
-  ],
-  pianist: [
-    {
-      label: 'Music by __QUERY',
-      facets: [
-        { facetType: 'mediatype', bucketKey: 'audio' },
-        { facetType: 'creator', bucketKey: '__QUERY' },
-      ],
-    },
-  ],
+  'written work': WRITTEN_WORK_SMART_FACETS,
+  literature: WRITTEN_WORK_SMART_FACETS,
+  book: WRITTEN_WORK_SMART_FACETS,
+  novel: WRITTEN_WORK_SMART_FACETS,
+
+  filmmaker: FILMMAKER_SMART_FACETS,
+  director: FILMMAKER_SMART_FACETS,
+
+  author: AUTHOR_SMART_FACETS,
+  writer: AUTHOR_SMART_FACETS,
+  novelist: AUTHOR_SMART_FACETS,
+  essayist: AUTHOR_SMART_FACETS,
+  poet: AUTHOR_SMART_FACETS,
+
+  'visual artist': VISUAL_ARTIST_SMART_FACETS,
+  'graphic artist': VISUAL_ARTIST_SMART_FACETS,
+  photographer: VISUAL_ARTIST_SMART_FACETS,
+  painter: VISUAL_ARTIST_SMART_FACETS,
+
+  singer: MUSICIAN_SMART_FACETS,
+  songwriter: MUSICIAN_SMART_FACETS,
+  musician: MUSICIAN_SMART_FACETS,
+  composer: MUSICIAN_SMART_FACETS,
+  pianist: MUSICIAN_SMART_FACETS,
 };
