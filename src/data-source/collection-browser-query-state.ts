1232b335190410c07ae781caa326cc5d86e2934f
Laton Vermette <1619661+latonv@users.noreply.github.com>
2025-07-04 12:30:52 -0700

    Check default sort applied by PPS instead of metadata

--- src/data-source/collection-browser-query-state.ts
+++ src/data-source/collection-browser-query-state.ts
@@ -49,7 +49,7 @@ export interface CollectionBrowserSearchInterface
   setFacetsLoading(loading: boolean): void;
   setTotalResultCount(count: number): void;
   setTileCount(count: number): void;
-  applyDefaultCollectionSort(collectionInfo?: CollectionExtraInfo): void;
+  applyDefaultCollectionSort(backendSort?: string): void;
   emitEmptyResults(): void;
   emitQueryStateChanged(): void;
   refreshVisibleResults(): void;
