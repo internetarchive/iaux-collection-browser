693d8388cebb1c0bd53d877b38047c1f13c7327d
Laton Vermette <1619661+latonv@users.noreply.github.com>
2026-02-05 00:19:27 -0800

    Expose 'minimal' layout type for collection tiles

--- src/tiles/models.ts
+++ src/tiles/models.ts
@@ -1,1 +1,12 @@
-export type SimpleLayoutType = 'none' | 'stats-only' | 'snippets-only';
+/**
+ * What type of simplified tile layout to use.
+ *  - `none`: Do not apply any layout simplifications.
+ *  - `stats-only`: Only show the tile stats row, but not text snippets.
+ *  - `snippets-only`: Only show the text snippets row (if snippets exist), but not tile stats.
+ *  - `minimal`: Show neither tile stats nor the text snippets.
+ */
+export type SimpleLayoutType =
+  | 'none'
+  | 'stats-only'
+  | 'snippets-only'
+  | 'minimal';
