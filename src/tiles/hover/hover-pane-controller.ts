2d978b4a771c7440809f244eaa49baa6dd3468c0
Laton Vermette <1619661+latonv@users.noreply.github.com>
2026-06-26 11:05:05 -0700

    Revert "Guard showPopover call for already-shown hover pane"
    This reverts commit 5dd96b07501f24d425c9bb5d8766df6f7c549099.

--- src/tiles/hover/hover-pane-controller.ts
+++ src/tiles/hover/hover-pane-controller.ts
@@ -581,9 +581,7 @@ export class HoverPaneController implements HoverPaneControllerInterface {
     // as it might have been removed by the previous update.
     if (!this.hoverPane?.isConnected) return;
 
-    if (this.hoverPane && !this.hoverPane.matches(':popover-open')) {
-      this.hoverPane.showPopover?.();
-    }
+    this.hoverPane?.showPopover?.();
     await new Promise(resolve => {
       // Pane sizes aren't accurate until next frame
       requestAnimationFrame(resolve);
